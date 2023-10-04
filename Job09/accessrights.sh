30fa9812cdf542e35e1c21250ce02c5d1c9302ff
theo-baccam <theo.baccam@laplateforme.io>
2023-10-04 21:19:49 +0200

    Mise à jour Job 09

--- Job09/accessrights.sh
+++ Job09/accessrights.sh
@@ -1,20 +1,20 @@
 path=$(echo $0 | rev | cut -b 17- | rev)
 modifsave=$(cat $path/modifsave)
-lastmodif=$(stat $path/Shell_Userlist.csv)
-if [[ $modifsave -ne $lastmodif ]]; then
+lastmodif=$(stat -c=%y $path/Shell_Userlist.csv)
+if [[ "$modifsave" != "$lastmodif" ]]; then
 	while IFS="," read -r id prenom nom mdp role
 	do
-		username=$(echo $prenom.$nom | tr '[:upper:]' '[:lower:]')
+		username=$(echo "$prenom.$nom" | tr '[:upper:]' '[:lower:]')
 		nrole=$(echo $role | tr -d '\r' | cat -t)
 		sudo useradd $username 
 		echo "$username:$mdp" | sudo chpasswd
-		if [ $nrole = Admin ]; then
+		if [ $nrole = "Admin" ]; then
 			sudo usermod -aG sudo $username
+			echo "did $username"
 		else
 			sudo usermod -aG users $username
+			echo "did $username"
 		fi
 	done < <(tail -n +2 Shell_Userlist.csv | tr -d " " && echo "")
 	stat -c=%y Shell_Userlist.csv > modifsave
-else
-	echo "all good!"
 fi
